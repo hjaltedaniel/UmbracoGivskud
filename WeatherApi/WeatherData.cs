520cdac5260913e19a78b8bbab7cd1b5a5d66be7
Hjalte Daniel Hansen <hej@hjaltedaniel.io>
2018-12-13 09:08:27 +0100

    Updating weather integration on front page

--- WeatherApi/WeatherData.cs
+++ WeatherApi/WeatherData.cs
@@ -7,7 +7,7 @@ namespace WeatherApi
     public class WeatherData
     {
         public DateTime dt_txt { get; set; }
-        public decimal temp { get; set; }
+        public int temp { get; set; }
         public string icon { get; set; }
     }
 }
