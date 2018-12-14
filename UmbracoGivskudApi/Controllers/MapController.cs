af72501e4806671f830b01f17ebf2537210a0477
Hjalte Daniel Hansen <hej@hjaltedaniel.io>
2018-12-14 10:08:50 +0100

    Creating web api

---
+++ UmbracoGivskudApi/Controllers/MapController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Umbraco.Core.Services;
+using Umbraco.Web.WebApi;
+using UmbracoGivskudApi.Models;
+
+namespace UmbracoGivskudApi.Controllers
+{
+    public class MapController : UmbracoApiController
+    {
+        [HttpGet]
+        public MapItem Get(int id)
+        {
+            IContentService cs = Services.ContentService;
+
+            var item = cs.GetById(id);
+
+            MapItem result = new MapItem();
+            result.Id = item.Id;
+            result.Title = item.GetValue<string>("title");
+            result.ContentText = item.GetValue<string>("contentText");
+            result.ImgSrc = item.GetValue<string>("image");
+            result.VideoLink = item.GetValue<string>("linkToVideo");
+
+            return result;
+        }
+    }
+}
---
+++ UmbracoGivskudApi/Models/MapItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UmbracoGivskudApi.Models
+{
+    public class MapItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ImgSrc { get; set; }
+        public string ContentText { get; set; }
+        public string VideoLink { get; set; }
+    }
+}
